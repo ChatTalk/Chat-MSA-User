6d9bf05d86b4238085cdeaf5b2348467e53a7918
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-07 17:39:55 +0900

    feat: entity migration

---
+++ src/main/java/com/example/chatserveruser/domain/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.chatserveruser.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private UserRoleEnum role;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+}
