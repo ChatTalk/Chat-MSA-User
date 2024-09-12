3ae51750391ba0d539c724c67191cb5a76916a6f
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-12 22:04:58 +0900

    fix: redis 저장 및 직접 조회 로직으로 수정

--- src/main/java/com/example/chatserveruser/global/kafka/KafkaListenerService.java
+++ src/main/java/com/example/chatserveruser/global/kafka/KafkaListenerService.java
@@ -1,17 +1,15 @@
 package com.example.chatserveruser.global.kafka;
 
-import com.example.chatserveruser.domain.dto.AuthorizationDTO;
 import com.example.chatserveruser.domain.dto.UserDTO;
 import com.example.chatserveruser.global.security.service.JwtTokenService;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import static com.example.chatserveruser.global.constant.Constants.KAFKA_OTHER_TO_USER_TOPIC;
-import static com.example.chatserveruser.global.constant.Constants.KAFKA_USER_TO_CHAT_TOPIC;
 
 @Slf4j
 @Service
@@ -19,7 +17,7 @@ import static com.example.chatserveruser.global.constant.Constants.KAFKA_USER_TO
 public class KafkaListenerService {
 
     private final JwtTokenService jwtTokenService;
-    private final KafkaTemplate<String, AuthorizationDTO> kafkaTemplate;
+    private final RedisTemplate<String, String> authTemplate;
 
     // 필요한 다른 곳에서 동일한 어노테이션을 할당하고 메세지를 받으면 됨
     @KafkaListener(topics = KAFKA_OTHER_TO_USER_TOPIC, groupId = "chat")
@@ -27,8 +25,10 @@ public class KafkaListenerService {
         log.info("수신한 날 것의 엑세스 토큰: {} ", beforeToken);
 
         // 레디스에 beforeToken - email 로 저장된 캐싱 조회해서 없으면 슉슉
-
-        if (beforeToken == null) throw new JwtException("엑세스 토큰이 존재하지 않습니다.");
+        if (Boolean.TRUE.equals(authTemplate.hasKey(beforeToken))) {
+            log.info("이미 해당 토큰이 캐싱되어 있음: {} ", beforeToken);
+            return;
+        }
 
         String tokenValue = jwtTokenService.extractValue(beforeToken);
         jwtTokenService.validAccessToken(tokenValue);
@@ -38,9 +38,7 @@ public class KafkaListenerService {
         UserDTO userDTO = jwtTokenService.getUserFromToken(tokenValue);
         String email = userDTO.getEmail();
 
-        AuthorizationDTO dto = new AuthorizationDTO(email, beforeToken);
-        kafkaTemplate.send(KAFKA_USER_TO_CHAT_TOPIC, dto);
-
-        log.info("카프카 송신, 이메일: {} // 토큰: {}", email, beforeToken);
+        authTemplate.opsForSet().add(beforeToken, email);
+        log.info("레디스 저장, 이메일: {} // 토큰: {}", email, beforeToken);
     }
 }
